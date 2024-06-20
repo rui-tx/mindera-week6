493fa6c45e0470c337eb8027909f162c5c4b9ef9
Rui Teixeira <ruiteixeira@minderacodeacademy.com>
2024-06-20 09:22:28 +0100

    added super simple todo list with server client udp connection

--- src/todoudp/Client.java
+++ src/todoudp/Client.java
@@ -1,4 +1,4 @@
-package quoteserver;
+package todoudp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,14 +9,16 @@ import java.net.InetAddress;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException {
+
+    private static void run() throws IOException {
         int serverPortNumber = 15000;
-        String hostName = "ducknexus.com"; // ducknexus.com
+        String hostName = "localhost"; // ducknexus.com
 
         DatagramSocket socket = new DatagramSocket();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String command;
         while (true) {
+
             System.out.print("[command]> ");
             command = reader.readLine();
 
@@ -28,14 +30,24 @@ public class Client {
                     command.getBytes(), command.getBytes().length,
                     InetAddress.getByName(hostName), serverPortNumber);
             socket.send(sendPacket);
-            System.out.println("[client]> sending packet to server " + hostName + ":" + serverPortNumber);
+            //System.out.println("[client]> sending packet to server " + hostName + ":" + serverPortNumber);
 
             byte[] recvBuffer = new byte[1024];
             DatagramPacket receivedPacket = new DatagramPacket(
                     recvBuffer, recvBuffer.length);
-            System.out.println("[client]> waiting for response...");
+            //System.out.println("[client]> waiting for response...");
             socket.receive(receivedPacket); // blocking method!
             System.out.println("[server]> " + new String(receivedPacket.getData(), 0, receivedPacket.getLength()));
         }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            run();
+        } catch (IOException e) {
+            System.out.println("[client]> Error: " + e.getMessage());
+        }
+
     }
 }
